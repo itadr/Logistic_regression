0adf0417064abff98ad0546065adfc13c5cc9641
itadr <41465121+itadr@users.noreply.github.com>
2020-08-10 02:16:14 +0900

    fit関数を書き加えたがまだ確かめていない

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
+#include <algorithm>
 #include <cmath>
 #include <fstream>
 #include <iostream>
+#include <random>
 #include <vector>
-#include<random>
 
 class logistic_regression {
  private:
@@ -33,15 +34,61 @@ void logistic_regression::fit(const std::vector<std::vector<double>>& x_train,
   // data_num is the number of train data
   const int data_num = x_train.size();
   // initialize all parameters to 0
-  params = std::vector<double>(params_num, 0);
+  params = std::vector<double>(params_num);
   // params_grad is used to calculate gradient of los function in each iteration
   std::vector<double> params_grad(params_num, 0);
   // data_order is used to shuffle train data for SGD
   std::vector<int> data_order(data_num);
+  std::random_device rnd;
+  std::mt19937 engine(rnd());
   // current_order is the number of data used in data_order
-  int current_order=0;
-  for(int i=0;i<max_iteration;i++){
+  int current_order = data_num, next_order;
 
+  for (int i = 0; i < max_iteration; i++) {
+    fill(params_grad.begin(), params.end(), 0);
+    next_order = current_order + batch_size;
+    // shuffling data is necessary
+    if (next_order >= data_num) {
+      for (int j = current_order; j < data_num; j++) {
+        const double exp_xw = exp(linear_prediction(x_train[data_order[j]]));
+        const double train_coeff =
+            y_train[data_order[j]] - exp_xw / (1 + exp_xw);
+        for (int k = 0; k < params_num; k++) {
+          params_grad[k] += train_coeff * x_train[data_order[j]][k];
+        }
+      }
+      // shuffle data 
+      std::shuffle(data_order.begin(), data_order.end(), engine);
+      next_order -= data_num;
+      for (int j = 0; j < next_order; j++) {
+        const double exp_xw = exp(linear_prediction(x_train[data_order[j]]));
+        const double train_coeff =
+            y_train[data_order[j]] - exp_xw / (1 + exp_xw);
+        for (int k = 0; k < params_num; k++) {
+          params_grad[k] += train_coeff * x_train[data_order[j]][k];
+        }
+      }
+    }
+    // shuffling data is not necessary
+    else {
+      for (int j = current_order; j < next_order; j++) {
+        const double exp_xw = exp(linear_prediction(x_train[data_order[j]]));
+        const double train_coeff =
+            y_train[data_order[j]] - exp_xw / (1 + exp_xw);
+        for (int k = 0; k < params_num; k++) {
+          params_grad[k] += train_coeff * x_train[data_order[j]][k];
+        }
+      }
+    }
+    for (int k = 0; k < params_num; k++) {
+      params_grad[k] =
+          params_grad[k] / (double)batch_size - penalty_coeff * params[k];
+    }
+    // update parameters of logistic regression
+    for (int k = 0; k < params_num; k++) {
+      params[k] = params[k] + learning_rate * params_grad[k];
+    }
+    current_order = next_order;
   }
 }
 double logistic_regression::linear_prediction(const std::vector<double>& x) {
